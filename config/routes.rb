2e148452d66bdd5caa5e6094613d38c4776b728c
Leticia Mayumi <hashizumemay@gmail.com>
2019-08-25 22:18:39 -0300

    Visitante visualiza receitas

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'recipes/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'recipes#index'
 
-  root 'recipes#index'
+  resources :recipes, only: %i[index show new create edit update]
 end
