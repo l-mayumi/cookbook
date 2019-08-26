af6dd264d1e377e997e88a9fd69db068a9bb13a5
Leticia Mayumi <hashizumemay@gmail.com>
2019-08-25 23:49:28 -0300

    WIP Usuario usa tipos de receita cadastradas

--- spec/features/user_edit_recipe_spec.rb
+++ spec/features/user_edit_recipe_spec.rb
@@ -2,10 +2,11 @@ require 'rails_helper'
 
 feature 'User update recipe' do
   scenario 'successfully' do
-    Recipe.create(title: 'Bolodecenoura', recipe_type: 'Sobremesa',
-                  cuisine: 'Brasileira', difficulty: 'Médio',
-                  cook_time: 50,
-                  ingredients: 'Farinha, açucar, cenoura',
+    recipe_type = RecipeType.create(name: 'Sobremesa')
+    RecipeType.create(name: 'Entrada')
+    Recipe.create(title: 'Bolodecenoura', difficulty: 'Médio',
+                  recipe_type: recipe_type, cuisine: 'Brasileira',
+                  cook_time: 50, ingredients: 'Farinha, açucar, cenoura',
                   cook_method: 'Cozinhe a cenoura, corte em pedaços pequenos, misture com o restante dos ingredientes')
 
     # simula a ação do usuário
@@ -14,8 +15,7 @@ feature 'User update recipe' do
     click_on 'Editar'
 
     fill_in 'Título', with: 'Bolo de cenoura'
-    fill_in 'Tipo da Receita', with: 'Sobremesa'
-    fill_in 'Cozinha', with: 'Brasileira'
+    select 'Entrada', from: 'Tipo da Receita'
     fill_in 'Dificuldade', with: 'Médio'
     fill_in 'Tempo de Preparo', with: '45'
     fill_in 'Ingredientes', with: 'Cenoura, farinha, ovo, oleo de soja e chocolate'
@@ -29,5 +29,28 @@ feature 'User update recipe' do
     expect(page).to have_css('p', text: '45 minutos')
     expect(page).to have_css('p', text:  'Cenoura, farinha, ovo, oleo de soja e chocolate')
     expect(page).to have_css('p', text: 'Faça um bolo e uma cobertura de chocolate')
+  end
+
+  scenario 'and must fill in all fields' do
+    recipe_type = RecipeType.create(name: 'Sobremesa')
+    Recipe.create(title: 'Bolodecenoura', difficulty: 'Médio',
+                  recipe_type: recipe_type, cuisine: 'Brasileira',
+                  cook_time: 50, ingredients: 'Farinha, açucar, cenoura',
+                  cook_method: 'Cozinhe a cenoura, corte em pedaços pequenos, misture com o restante dos ingredientes')
+
+    # simula a ação do usuário
+    visit root_path
+    click_on 'Bolodecenoura'
+    click_on 'Editar'
+
+    fill_in 'Título', with: ''
+    fill_in 'Cozinha', with: ''
+    fill_in 'Dificuldade', with: ''
+    fill_in 'Tempo de Preparo', with: ''
+    fill_in 'Ingredientes', with: ''
+    fill_in 'Como Preparar', with: ''
+    click_on 'Enviar'
+
+    expect(page).to have_content('Não foi possível salvar a receita')
   end
 end
