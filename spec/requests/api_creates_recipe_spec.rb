a71dd1bf01059bd63eff00102727bc9c8fba5058
Leticia Mayumi <hashizumemay@gmail.com>
2019-09-11 03:11:12 -0300

    Api registers recipe - improving test scenarios

--- spec/requests/api_creates_recipe_spec.rb
+++ spec/requests/api_creates_recipe_spec.rb
@@ -22,21 +22,43 @@ describe 'API creates recipe' do
   end
 
   it 'and must fill in all fields' do 
-    user = User.create(email: 'user0@email.com', password: '123456')
-    recipe_type = RecipeType.create(name: 'Sobremesa')
-    cuisine = Cuisine.create(name: 'Brasileira')
-    file = fixture_file_upload("recipe.jpeg")
-    recipe = {recipe: {title: '', recipe_type: recipe_type,
-                        difficulty: '', cuisine: cuisine, recipe_photo: file,
+    recipe = {recipe: {title: '', recipe_type_id: '',
+                        difficulty: '', cuisine_id: '',
                         cook_time: 50, ingredients: '',
                         cook_method: '',
-                        user: user, status: :approved}}
+                        user_id: '1' }}
 
     post api_v1_recipes_path(recipe)
 
     json_recipe = JSON.parse(response.body, symbolize_names: true)
 
-    expect(response.status).to eq 404
+    expect(response.status).to eq 406
+    expect(json_recipe[:message]).to eq 'Erro ao salvar tipo de receita'
+  end
+
+  it 'and it should have an unique name' do 
+    user = User.create(email: 'user0@email.com', password: '123456')
+    recipe_type = RecipeType.create(name: 'Sobremesa')
+    cuisine = Cuisine.create(name: 'Brasileira')
+    recipe_photo = fixture_file_upload("recipe.jpeg")
+    Recipe.create(title: 'Bolo de cenoura', recipe_type: recipe_type,
+                           cuisine: cuisine, difficulty: 'Médio',
+                           cook_time: 60, recipe_photo: recipe_photo,
+                           ingredients: 'Farinha, açucar, cenoura',
+                           cook_method: 'Cozinhe a cenoura, corte em pedaços pequenos,'\
+                           ' misture com o restante dos ingredientes', user: user)
+    recipe = {recipe: {title: 'Bolo de cenoura', recipe_type_id: 1,
+                           difficulty: 'Médio', cuisine_id: 1,
+                           cook_time: 50, ingredients: 'Farinha, açucar, cenoura',
+                           cook_method: 'Cozinhe a cenoura, corte em pedaços pequenos, misture com o restante dos ingredientes',
+                           user_id: '1'}}
+
+    post api_v1_recipes_path(recipe)
+
+    json_recipe = JSON.parse(response.body, symbolize_names: true)
+    
+
+    expect(response.status).to eq 406
     expect(json_recipe[:message]).to eq 'Erro ao salvar tipo de receita'
   end
 end
